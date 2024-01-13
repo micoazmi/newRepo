b4ad0a0a5234404d402fa3204372ce4eae8a06eb
unknown <black.samurai6@gmail.com>
2024-01-13 21:44:40 +0700

    fix

--- my-app/src/action/handleLogout.ts
+++
@@ -1,8 +0,0 @@
-import { useRouter } from 'next/router';
-import Cookies from 'js-cookie';
-
-export async function handleLogout() {
-    const router = useRouter();
-    Cookies.remove('Authorization');
-    router.push('/login');
-  }
